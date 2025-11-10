8f9ab0363c5e5e42828c9e4fc35af3a7944152b1
enger920-dev <enger920@nmsu.edu>
2025-11-10 00:27:14 -0700

    js first test

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const clips = [
   'audio/alcaholic.mp3',
   'audio/ipfreely.mp3',
-  'audio/jaquesstrap.mp3'
-  'audio/kissmyownbutt.mp3'
-  'audio/mikerotch.mp3'
+  'audio/jaquesstrap.mp3',
+  'audio/kissmyownbutt.mp3',
+  'audio/mikerotch.mp3',
   'audio/seymourbutts.mp3'
 ];
 
@@ -21,5 +21,5 @@ document.getElementById('playButton').addEventListener('click', () => {
   currentAudio = new Audio(randomClip);
   currentAudio.play();
 
-  document.getElementById('nowPlaying').textContent = `Now playing: ${randomClip}`;
+  
 });
